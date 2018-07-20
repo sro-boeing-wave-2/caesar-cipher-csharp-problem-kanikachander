136e4958bd93f7a7d74ff51338dc8c9f071c8522
Kanika <kanikachander1@gmail.com>
2018-07-20 15:59:02 +0530

    Done

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,6 +7,31 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            char[] ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+             
+            string str = "";
+            foreach(var i in text)
+            {
+                if(alphabet.Contains(i))
+                {
+                    var index = Array.IndexOf(alphabet, i);
+                    index = (index + shiftKey) % 26;
+                    
+                    str += alphabet[index];
+                }
+                else if(ALPHABET.Contains(i))
+                {
+                    var index = Array.IndexOf(ALPHABET, i);
+                    index = (index + shiftKey) % 26;
+                    str += ALPHABET[index];
+                }
+                else
+                {
+                    str += i;
+                }
+            }
+            return str;
             throw new NotImplementedException("You need to implement this function.");
         }
     }
